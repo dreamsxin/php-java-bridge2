2097568d43b48d423caf4830c9254144eb7f6e05
jost_boekemeier <jost_boekemeier@2b3e608b-75ca-43f1-bfa3-6c9ec92cc88d>
2017-02-05 19:56:40 +0000

    added source

--- trunk/php-java-bridge/CVS_RELEASE.sh
+++ trunk/php-java-bridge/CVS_RELEASE.sh
@@ -61,9 +61,9 @@ mv JavaBridgeTemplate.war "JavaBridgeTemplate`echo ${version}|sed 's/\.//g'`.war
 rm -rf $dirs
 cvs -z9 -Q update -APd 
 
-ssh -p $port "jost_boekemeier@${host}.sourceforge.net" mkdir -p "/home/frs/project/php-java-bridge/Binary\ package/php-java-bridge_`cat VERSION`/exploded/"
+ssh -p $port "jost_boekemeier@${host}.sourceforge.net" mkdir -p "/home/frs/project/php-java-bridge/Binary\ package/php-java-bridge_`cat VERSION`/exploded/ /home/frs/project/php-java-bridge/src/php-java-bridge_`cat VERSION`/"
 scp -P $port  "php-java-bridge_`cat VERSION`_documentation.zip" "JavaBridgeTemplate`echo ${version}|sed 's/\.//g'`.war" "jost_boekemeier@${host}.sourceforge.net:/home/frs/project/php-java-bridge/Binary\ package/php-java-bridge_`cat VERSION`/"
 
 scp -P $port  dist/Java.inc dist/php-script.jar dist/JavaBridge.jar dist/php-servlet.jar dist/script-api.jar "jost_boekemeier@${host}.sourceforge.net:/home/frs/project/php-java-bridge/Binary\ package/php-java-bridge_`cat VERSION`/exploded/"
 
-#scp "php-java-bridge_`cat VERSION`.tar.gz" jost_boekemeier,php-java-bridge@web.sf.net:"/home/pfs/project/p/ph/php-java-bridge/RHEL_FC\ SecurityEnhancedLinux/php-java-bridge_`cat VERSION`/"
+scp "php-java-bridge_`cat VERSION`.tar.gz" jost_boekemeier,php-java-bridge@web.sf.net:"/home/frs/project/php-java-bridge/src/php-java-bridge_`cat VERSION`/"
