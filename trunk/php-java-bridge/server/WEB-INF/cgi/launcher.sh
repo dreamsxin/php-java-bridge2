09b7d894f5b3d740992faaae2c592ed6d7c2feb5
jost_boekemeier <jost_boekemeier@2b3e608b-75ca-43f1-bfa3-6c9ec92cc88d>
2017-03-19 18:22:24 +0000

    New FastCGI implementation.

--- trunk/php-java-bridge/server/WEB-INF/cgi/launcher.sh
+++ trunk/php-java-bridge/server/WEB-INF/cgi/launcher.sh
@@ -2,7 +2,9 @@
 # php fcgi launcher
 set -x
 
-export PHP_FCGI_CHILDREN="$PHP_JAVA_BRIDGE_FCGI_CHILDREN"
+PHP_FCGI_CHILDREN="$PHP_JAVA_BRIDGE_FCGI_CHILDREN"
+export PHP_FCGI_CHILDREN
+
 "$@" 1>&2 &
 trap "kill $! && exit 0;" 1 2 15
 read result 1>&2
