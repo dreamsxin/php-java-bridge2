1f1fdb167174e99bc565bddf8e4365f713d5ba60
jost_boekemeier <jost_boekemeier@2b3e608b-75ca-43f1-bfa3-6c9ec92cc88d>
2017-04-16 10:22:33 +0000

    Release-7.1.2

--- trunk/php-java-bridge/CVS_RELEASE.sh
+++ trunk/php-java-bridge/CVS_RELEASE.sh
@@ -8,33 +8,32 @@ read port
 echo "host (example: shell4):"
 read host
 
-rm -rf [^C][^V][^S]* .??* *~
-cvs -z9 -Q update -APd 
+version=`cat VERSION`
+
+rm -rf .??* *~
+( cd ..; tar czf php-java-bridge_${version}.tar.gz php-java-bridge )
 find . -print0 | xargs -0 touch
 dirs=`ls -l | grep '^d' | fgrep -v CVS | awk '{print $9}'`
 find $dirs -name "CVS" -print | xargs rm -rf
 
 cp WebContent/test.php .
-version=`cat VERSION`
 ln -s `pwd` php-java-bridge-${version}
 
 # create archive
 tar czhf php-java-bridge_${version}.tar.gz --exclude "php-java-bridge-${version}/php-java-bridge[_-]*" --exclude CVS --exclude ".??*" php-java-bridge-${version}
 
 rpmbuild -tb php-java-bridge_${version}.tar.gz
-mv ~/rpmbuild/RPMS/i386/php-java-bridge-${version}-1.i386.rpm "./php-java-bridge-${version}-1.fc`cat /etc/issue | sed 1q | awk '{print $3}'`.i386.rpm"
-mv ~/rpmbuild/RPMS/i386/php-java-bridge-devel-${version}-1.i386.rpm "./php-java-bridge-devel-${version}-1.fc`cat /etc/issue | sed 1q | awk '{print $3}'`.i386.rpm"
 
-ant clean &&
-ant PhpDoc 2>/dev/null >/dev/null && 
-ant && 
+ant clean
+#ant PhpDoc 2>/dev/null >/dev/null && 
+ant 
 ant SrcZip
 
 cp dist/*.war dist/src.zip .
 cp -r php_java_lib tests.php5 tests.jsr223 server
 
 cp JavaBridge.war JavaBridgeTemplate.war
-for i in 'META-INF/*' 'WEB-INF/lib/[^pJ]*.jar' 'WEB-INF/lib/poi.jar' 'WEB-INF/cgi/*' 'WEB-INF/web.xml' 'WEB-INF/platform/*' 'locale/*' '*.class' '*.jsp' '*.rpt*' '*.php'; do
+for i in 'org/' 'META-INF/*' 'WEB-INF/lib/[^pJ]*.jar' 'WEB-INF/lib/poi.jar' 'WEB-INF/cgi/*' 'WEB-INF/web.xml' 'WEB-INF/platform/*' 'locale/*' '*.class' '*.jsp' '*.rpt*' '*.php'; do
   zip -d JavaBridgeTemplate.war "$i"; 
 done
 cat WebContent/settings.php >./index.php
@@ -55,14 +54,15 @@ find $list -type d -name "CVS" -print | xargs rm -rf
 chmod +x JavaBridge.war
 
 # create j2ee download
-zip -q -r php-java-bridge_${version}_documentation.zip $list
 mv JavaBridgeTemplate.war "JavaBridgeTemplate`echo ${version}|sed 's/\.//g'`.war"
-rm -rf $dirs
-cvs -z9 -Q update -APd 
 
 ssh -p $port "jost_boekemeier@${host}.sourceforge.net" mkdir -p "/home/frs/project/php-java-bridge/Binary\ package/php-java-bridge_`cat VERSION`/exploded/ /home/frs/project/php-java-bridge/src/php-java-bridge_`cat VERSION`/"
-scp -P $port  "php-java-bridge_`cat VERSION`_documentation.zip" "JavaBridgeTemplate`echo ${version}|sed 's/\.//g'`.war" "jost_boekemeier@${host}.sourceforge.net:/home/frs/project/php-java-bridge/Binary\ package/php-java-bridge_`cat VERSION`/"
+scp -P $port  "JavaBridge.war" "JavaBridgeTemplate`echo ${version}|sed 's/\.//g'`.war" "jost_boekemeier@${host}.sourceforge.net:/home/frs/project/php-java-bridge/Binary\ package/php-java-bridge_`cat VERSION`/"
 
 scp -P $port  dist/Java.inc dist/JavaBridge.jar dist/php-servlet.jar "jost_boekemeier@${host}.sourceforge.net:/home/frs/project/php-java-bridge/Binary\ package/php-java-bridge_`cat VERSION`/exploded/"
 
 scp "php-java-bridge_`cat VERSION`.tar.gz" jost_boekemeier,php-java-bridge@web.sf.net:"/home/frs/project/php-java-bridge/src/php-java-bridge_`cat VERSION`/"
+
+echo "done. type return to restore modified contents"
+read a
+( cd ..; rm -rf php-java-bridge; tar xzf php-java-bridge_${version}.tar.gz )
