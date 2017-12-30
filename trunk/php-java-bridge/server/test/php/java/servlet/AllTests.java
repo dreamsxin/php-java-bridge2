df91a4b8f76d3e0e0a74217e3c91a54839ea12e5
jost_boekemeier <jost_boekemeier@2b3e608b-75ca-43f1-bfa3-6c9ec92cc88d>
2017-12-30 13:35:28 +0000

    * server/test/php/java/servlet/TestServletUtil.java: new file * server/php/java/servlet/ServletUtil.java (getRealPath): Check NP.

---
+++ trunk/php-java-bridge/server/test/php/java/servlet/AllTests.java
@@ -0,0 +1,11 @@
+package test.php.java.servlet;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestServletUtil.class })
+public class AllTests {
+
+}
