ace02222064579a78e99e51c4a94bfb2fa774f7a
jost_boekemeier <jost_boekemeier@2b3e608b-75ca-43f1-bfa3-6c9ec92cc88d>
2018-01-02 17:11:06 +0000

    Do not let two PHP script engines sharing the same script run concurrently.

--- trunk/php-java-bridge/server/php/java/test/AllTests.java
+++ trunk/php-java-bridge/server/php/java/test/AllTests.java
@@ -7,13 +7,14 @@ import org.junit.runners.Suite.SuiteClasses;
 import php.java.test.php5.TestExtends;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TestBindings.class, TestCGI.class, TestCli.class,
-        TestDiscovery.class, TestException.class, TestExceptionInvocable2.class,
-        TestGetInterface.class, TestGetResult.class, TestError.class,
-        TestInvocable.class, TestInvocablePhpScriptEngine.class,
-        TestPhpScriptEngine.class, TestScript.class, TestSetWriter.class,
-        TestSimpleCompileable.class, TestSimpleInvocation.class,
-        TestExtends.class, InterfaceWithDefaultMethod.class, TestSystemExit.class,
+@SuiteClasses({ test.php.java.servlet.AllTests.class, TestBindings.class,
+        TestCGI.class, TestCli.class, TestDiscovery.class, TestException.class,
+        TestExceptionInvocable2.class, TestGetInterface.class,
+        TestGetResult.class, TestError.class, TestInvocable.class,
+        TestInvocablePhpScriptEngine.class, TestPhpScriptEngine.class,
+        TestScript.class, TestSetWriter.class, TestSimpleCompileable.class,
+        TestSimpleInvocation.class, TestExtends.class,
+        InterfaceWithDefaultMethod.class, TestSystemExit.class,
         TestConnectionPool.class })
 public class AllTests {
 
